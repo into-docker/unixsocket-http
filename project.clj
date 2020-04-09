b64955adefd113f1378aca9f4b38ce5dc96ce2ce
Yannick Scherer <yannick.scherer@futurice.com>
2020-04-09 17:37:39 +0200

    Version 1.0.1-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject unixsocket-http "1.0.0"
+(defproject unixsocket-http "1.0.1-SNAPSHOT"
   :description "A library to allow HTTP calls over a UNIX socket, e.g. for
                 communicating with Docker."
   :url "https://github.com/into-docker/unixsocket-http"
