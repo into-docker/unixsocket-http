707f2153b59dadc519df258d190a6c0746dc2926
Yannick Scherer <yannick.scherer@futurice.com>
2020-04-18 14:55:25 +0200

    Add SingletonSocketFactory
    We need to get access to the socket used by the OkHttpClient. Since
    there is no way to get it out of the client, we need to have a
    SocketFactory that creates a single socket only and lets us access it.

--- project.clj
+++ project.clj
@@ -16,6 +16,7 @@
         unixsocket-http.impl.FixedPathUnixSocketFactory
         unixsocket-http.impl.FixedPathTcpSocket
         unixsocket-http.impl.FixedPathTcpSocketFactory
+        unixsocket-http.impl.SingletonSocketFactory
         unixsocket-http.impl.StreamingBody]
   :profiles {:dev {:dependencies [[org.nanohttpd/nanohttpd "2.3.1"]
                                   [org.clojure/test.check "1.0.0"]
---
+++ src/unixsocket_http/impl/SingletonSocketFactory.clj
@@ -0,0 +1,41 @@
+(ns unixsocket-http.impl.SingletonSocketFactory
+  "SocketFactory that produces `FixedPathTcpSocket` objects bound to a fixed path."
+  (:gen-class
+    :name            unixsocket_http.impl.SingletonSocketFactory
+    :extends         javax.net.SocketFactory
+    :init            init
+    :state           socket
+    :constructors    {[javax.net.SocketFactory] []}
+    :methods         [[getSocket [] java.net.Socket]])
+  (:import [java.net InetAddress InetSocketAddress Socket]
+           [javax.net SocketFactory]))
+
+;; ## Constructor
+
+(defn -init
+  [^SocketFactory factory]
+  [[] (delay (.createSocket factory))])
+
+;; ## Methods
+
+(defn- get-socket
+  ^Socket [^unixsocket_http.impl.SingletonSocketFactory this]
+  @(.-socket this))
+
+(defn -createSocket
+  ([this]
+   (get-socket this))
+  ([this ^InetAddress _ ^Integer _]
+   (doto (get-socket this)
+     (.connect (InetSocketAddress. 0))))
+  ([this ^InetAddress _ ^Integer _ ^InetAddress _ ^Integer _]
+   (doto (get-socket this)
+     (.connect (InetSocketAddress. 0)))))
+
+(defn -getSocket
+  [this]
+  (get-socket this))
+
+(defn -toString
+  [^unixsocket_http.impl.SingletonSocketFactory this]
+  (str (.-socket this)))
